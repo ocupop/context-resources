2aad62f7f7a69b1d9a8e00cff43c28794c65705c
tombeck <t@ocupop.com>
2025-11-19 04:54:36 -0800

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import bookshop from "@bookshop/astro-bookshop";
+import mdx from "@astrojs/mdx";
+import alpine from "@astrojs/alpinejs";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://tiny-jackal.cloudvent.net/", // Replace this with your site's own custom url
+  integrations: [react(), bookshop(), alpine(), mdx()],
+});
